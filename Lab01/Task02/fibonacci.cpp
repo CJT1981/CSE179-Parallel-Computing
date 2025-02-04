5724ce2f2a72d738e83816b8aa662bbd50d6869c
CJT1981 <ctapia5@ucmerced.edu>
2025-02-04 15:46:47 -0800

    organization

--- Lab01/Task02/fibonacci.cpp
+++ Lab01/Task02/fibonacci.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     int n, firstTerm = 1, secondTerm = 1, nextTerm;
     cout << "Enter number of terms: ";
     cin >> n;
 
     cout << "Fibonacci Series: " << firstTerm << " + " << secondTerm << " + ";
 
-    //measure the execution time of the following loop
-    for (int i = 1; i <= n-2; ++i) {
+    // measure the execution time of the following loop
+    for (int i = 1; i <= n - 2; ++i)
+    {
         nextTerm = firstTerm + secondTerm;
         cout << nextTerm << " + ";
         firstTerm = secondTerm;
