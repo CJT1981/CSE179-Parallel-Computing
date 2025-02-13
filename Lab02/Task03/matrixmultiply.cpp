cd87596025d729902873f2c50aa8301f2e2e9c8d
CJT1981 <ctapia5@ucmerced.edu>
2025-02-13 14:20:49 -0800

    matrix multipication completed
    parallelized code completed but still need to measure performance

--- Lab02/Task03/matrixmultiply.cpp
+++ Lab02/Task03/matrixmultiply.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,10 +15,57 @@ int main()
   double a[matrix_A_Rows][matrix_A_Columns],
       b[matrix_A_Columns][matrix_B_Columns],
       c[matrix_A_Rows][matrix_B_Columns];
-  chunk = 15;
+  chunk = 4;
 
 #pragma omp parallel shared(a, b, c, num_of_threads, chunk) private(thread_id, i, j, k)
   {
     thread_id = omp_get_thread_num();
+    if (thread_id == 0) // <-- master thread
+    {
+      // Get number of threads based on export line in terminal
+      num_of_threads = omp_get_num_threads();
+      cout << "Starting Matrix Multiplication, with " << num_of_threads << " threads\n";
     }
+    // Initializing matrices
+    #pragma omp for schedule(static, chunk)
+    for (i = 0; i < matrix_A_Rows; i++)
+    {
+      for (j = 0; j < matrix_A_Columns; j++)
+      {
+        a[i][j] = i + j;
+      } 
+    }
+
+    #pragma omp for schedule(static, chunk)
+    for (i = 0; i < matrix_A_Columns; i++)
+    {
+      for (j = 0; j < matrix_B_Columns; j++)
+      {
+        b[i][j] = i * j;
+      }
+    }
+      
+    #pragma omp for schedule(static, chunk) 
+    for (i = 0; i < matrix_A_Rows; i++)
+    {
+      for (j = 0; j < matrix_B_Columns; j++)
+      {
+        c[i][j] = 0;
+      }
+    }
+
+    #pragma omp critical
+    cout << "Thread " << thread_id << " starting matrix multiply:\n";
+    
+    #pragma omp for schedule(static, chunk)
+      for (i = 0; i < matrix_A_Rows; i++)
+      {
+        #pragma omp critical
+        cout << "Thread: " << thread_id << " completed row " << i << endl;
+
+        for (j = 0; j < matrix_B_Columns; j++)
+          for (k = 0; k < matrix_A_Columns; k++)
+            c[i][j] += a[i][k] * b[k][j];
+      }
+  } // end of parallel work
 }
